04f3e924a4c63e353c0f80c37aacf009ae4d6b99
sondt0205 <daotruongson2006@gmail.com>
2024-12-25 13:51:10 +0700

    Update Session-21_Ex-07.c

--- Session-21_Ex-07.c
+++ Session-21_Ex-07.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// Khai báo cấu trúc sinh viên
 struct Student {
     int id;
     char name[50];
@@ -14,20 +12,23 @@ int main() {
     FILE *file;
     printf("Nhập số lượng sinh viên: ");
     scanf("%d", &n);
+    
     struct Student *students = (struct Student *)malloc(n * sizeof(struct Student));
     for (int i = 0; i < n; i++) {
         printf("\nNhập thông tin cho sinh viên thứ %d:\n", i + 1);
         printf("ID: ");
-        scanf("%d", &students[i].id);
+        scanf("%d",&students[i].id);
         printf("Tên: ");
         getchar();
         fgets(students[i].name, 50, stdin);
+        students[i].name[strcspn(students[i].name, "\n")] = '\0';
         printf("Tuổi: ");
         scanf("%d", &students[i].age);
     }
     file = fopen("students.txt", "w");
+    fprintf(file,"%d\n",n);
     for (int i = 0; i < n; i++) {
-        fprintf(file, "%d, %s, %d\n", students[i].id, students[i].name, students[i].age);
+        fprintf(file, "%d %s %d\n", students[i].id, students[i].name, students[i].age);
     }
     fclose(file);
 }
